5866e3c6786048f4a33e863fa5cc75e794c5db13
Y- terashita <y-terashita4278@st.kyoto-art.ac.jp>
2020-11-12 22:13:29 +0900

    kadai2

--- work/js/script.js
+++ work/js/script.js
@@ -7,5 +7,37 @@ $(function(){
   $(".close_button").on("click",function(){
       $(this).parent(".content").slideUp();
   })
+
+  let windowH
+  let documentH 
+  let documentW 
+  let scrollTop
+  let separate
+  let scrollRatio
   
+  $(window).on("scroll resize load",function(){
+    windowH = $(this).height()
+    documentH = $(document).height()
+    documentW = $(document).width()
+    scrollTop = $(this).scrollTop()
+    scrollRatio = scrollTop/(documentH-windowH)
+      
+    separate = documentH / 3;
+  
+    if(scrollTop < separate){
+      $("body").removeClass().addClass("one")
+    }else if(scrollTop < separate * 2){
+      $("body").removeClass().addClass("two")
+    }else{
+      $("body").removeClass().addClass("three")
+    }
+  
+    console.log(windowH,documentH,scrollTop)
+  
+    $("#wave").css("background-position-x",scrollTop/10)
+    $("#line").css("width",scrollRatio * documentW)
+  
+  })
+  
+
 });
